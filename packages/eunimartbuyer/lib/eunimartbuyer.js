57b6b46fddf5c638fc2169f7a260675befc31a00
madhu-Eunimart <madhu.varma@eunimart.com>
2023-05-19 10:52:24 +0530

    new package

--- packages/eunimartbuyer/lib/eunimartbuyer.js
+++ packages/eunimartbuyer/lib/eunimartbuyer.js
@@ -1,8 +1,25 @@
-'use strict';
-import Test from "./src/";
-module.exports = eunimartbuyer;
+import { Config } from "./src/dist/buyer/config/config.js";
+import dotenv from 'dotenv'
+import { eventEmitter } from "./src/dist/emitter/emitter.js";
+import Order from "./src/dist/api_methods/order.js";
 
-function eunimartbuyer() {
-  console(Test)
-  return 'Hello from eunimartbuyer';
+dotenv.config();
+
+
+class eunimartbuyer {
+  constructor(key_id, secret_key) {
+    this.key_id = key_id
+    this.secret_key = secret_key
+    this.config = new Config(key_id, secret_key)
+    this.order = new Order(key_id, secret_key)
+    this.emitter = eventEmitter
+  }
+  Config(config) {
+    // this.config.DbConfig(data.uri)
+    this.config.SdkConfig(config)
+  }
+  Router() {
+    return this.config.RouterExport()
+  }
 }
+export default eunimartbuyer
